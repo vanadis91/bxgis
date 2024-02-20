ee0fda9b7f0626efbb36a14b61de73389cdd864d
vanadis91 <vanadis91@163.com>
2024-02-21 07:33:08 +0800

    2024-02-21,07:33:01

--- src/bxgis/common/bxarcpy/游标类.py
+++ src/bxgis/common/bxarcpy/游标类.py
@@ -61,99 +61,99 @@ class 游标类:
     #                 用于更新的列表.append(v)
     #         return self._内嵌对象_游标对象._内嵌对象.insertRow(用于更新的列表)
 
-    class 形状类:
-        def __init__(self, 内嵌对象) -> None:
-            # print(f"形状类：{内嵌对象.__class__}")
-            self._内嵌对象 = 内嵌对象
+    # class 形状类:
+    #     def __init__(self, 内嵌对象) -> None:
+    #         # print(f"形状类：{内嵌对象.__class__}")
+    #         self._内嵌对象 = 内嵌对象
 
-        @property
-        def 类型(self):
-            类型 = self._内嵌对象.type
-            return _要素类型反映射[类型.upper()]
+    #     @property
+    #     def 类型(self):
+    #         类型 = self._内嵌对象.type
+    #         return _要素类型反映射[类型.upper()]
 
-        @property
-        def 点表(self):
-            return self._内嵌对象.getPart()
+    #     @property
+    #     def 点表(self):
+    #         return self._内嵌对象.getPart()
 
-        @property
-        def json格式(self):
-            import json
+    #     @property
+    #     def json格式(self):
+    #         import json
 
-            return json.loads(self._内嵌对象.JSON)
+    #         return json.loads(self._内嵌对象.JSON)
 
-        def 交集(self, 相交要素形状: "游标类.形状类", 类型="面"):
-            _交集类型映射表 = {4: 4, "面": 4, 2: 2, "线": 2, 1: 1, "点": 1}
-            ret = self._内嵌对象.intersect(相交要素形状._内嵌对象, _交集类型映射表[类型])
-            if ret:
-                return 游标类.形状类(ret)
-            else:
-                return ret
+    #     def 交集(self, 相交要素形状: "游标类.形状类", 类型="面"):
+    #         _交集类型映射表 = {4: 4, "面": 4, 2: 2, "线": 2, 1: 1, "点": 1}
+    #         ret = self._内嵌对象.intersect(相交要素形状._内嵌对象, _交集类型映射表[类型])
+    #         if ret:
+    #             return 游标类.形状类(ret)
+    #         else:
+    #             return ret
 
-        def 差集(self, 擦除要素形状: "游标类.形状类"):
-            ret = self._内嵌对象.difference(擦除要素形状._内嵌对象)
-            if ret:
-                return 游标类.形状类(ret)
-            else:
-                return ret
+    #     def 差集(self, 擦除要素形状: "游标类.形状类"):
+    #         ret = self._内嵌对象.difference(擦除要素形状._内嵌对象)
+    #         if ret:
+    #             return 游标类.形状类(ret)
+    #         else:
+    #             return ret
 
-        def 并集(self, 合并要素形状: "游标类.形状类"):
-            ret = self._内嵌对象.union(合并要素形状._内嵌对象)
-            if ret:
-                return 游标类.形状类(ret)
-            else:
-                return ret
+    #     def 并集(self, 合并要素形状: "游标类.形状类"):
+    #         ret = self._内嵌对象.union(合并要素形状._内嵌对象)
+    #         if ret:
+    #             return 游标类.形状类(ret)
+    #         else:
+    #             return ret
 
-        # def 转线(self, 输出要素名称):
-        #     arcpy.PolygonToLine_management(self._内嵌对象, neighbor_option="IGNORE_NEIGHBORS")
+    #     # def 转线(self, 输出要素名称):
+    #     #     arcpy.PolygonToLine_management(self._内嵌对象, neighbor_option="IGNORE_NEIGHBORS")
 
-        @property
-        def 内点(self):
-            return self._内嵌对象.labelPoint
+    #     @property
+    #     def 内点(self):
+    #         return self._内嵌对象.labelPoint
 
-        @property
-        def 部件数量(self):
-            return self._内嵌对象.partCount
+    #     @property
+    #     def 部件数量(self):
+    #         return self._内嵌对象.partCount
 
-        @property
-        def 折点数量(self):
-            return self._内嵌对象.pointCount
+    #     @property
+    #     def 折点数量(self):
+    #         return self._内嵌对象.pointCount
 
-        @property
-        def 面积(self):
-            return self._内嵌对象.area
+    #     @property
+    #     def 面积(self):
+    #         return self._内嵌对象.area
 
-        def 边界获取(self):
-            return 游标类.形状类(self._内嵌对象.boundary())
+    #     def 边界获取(self):
+    #         return 游标类.形状类(self._内嵌对象.boundary())
 
-        def 是否具有孔洞(self):
-            return self._内嵌对象.hasOmittedBoundary
+    #     def 是否具有孔洞(self):
+    #         return self._内嵌对象.hasOmittedBoundary
 
-        def 是否为多部件要素(self):
-            return self._内嵌对象.isMultipart
+    #     def 是否为多部件要素(self):
+    #         return self._内嵌对象.isMultipart
 
-        def 是否包含曲线(self):
-            ret = [x for x in self.json格式.keys() if "curve" in x]
-            if len(ret) > 0:
-                return True
-            return False
+    #     def 是否包含曲线(self):
+    #         ret = [x for x in self.json格式.keys() if "curve" in x]
+    #         if len(ret) > 0:
+    #             return True
+    #         return False
 
-        @property
-        def 孔洞数量(self):
-            折点和孔洞数量总数 = 0
-            for 每个部件x in self._内嵌对象.getPart():
-                折点和孔洞数量 = len(每个部件x)
-                折点和孔洞数量总数 += 折点和孔洞数量
-            return 折点和孔洞数量总数 - self.折点数量
+    #     @property
+    #     def 孔洞数量(self):
+    #         折点和孔洞数量总数 = 0
+    #         for 每个部件x in self._内嵌对象.getPart():
+    #             折点和孔洞数量 = len(每个部件x)
+    #             折点和孔洞数量总数 += 折点和孔洞数量
+    #         return 折点和孔洞数量总数 - self.折点数量
 
-        # @property
-        # def 孔洞数量(self):
-        #     return self._内嵌对象.interiorRingCount()
+    #     # @property
+    #     # def 孔洞数量(self):
+    #     #     return self._内嵌对象.interiorRingCount()
 
-        # def 边界部件获取(self, 索引):
-        #     return self._内嵌对象[索引]
+    #     # def 边界部件获取(self, 索引):
+    #     #     return self._内嵌对象[索引]
 
-        # def 边界组成数量(self):
-        #     return self._内嵌对象.getPartCount()
+    #     # def 边界组成数量(self):
+    #     #     return self._内嵌对象.getPartCount()
 
     @staticmethod
     def 游标创建_通过名称(游标类型, 输入要素名称, 需操作的字段名称列表: list):
@@ -201,15 +201,8 @@ class 游标类:
     def __next__(self):
         行数据 = self._内嵌对象.__next__()
         # 行对象 = 游标类.行对象类(行数据, self)
-        行数据temp = []
-        for k, v in zip(self.字段名称列表, 行数据):
-            if k == "_形状":
-                行数据temp.append(游标类.形状类(v))
-            else:
-                行数据temp.append(v)
-        行数据temp = {k: v for k, v in zip(self.字段名称列表, 行数据temp)}
         # print(f"行数据temp：{行数据temp}")
-        return 行数据temp
+        return {k: v for k, v in zip(self.字段名称列表, 行数据)}
 
     def 下一个(self):
         return self.__next__()
@@ -224,32 +217,20 @@ class 游标类:
         #     else:
         #         行数据temp.append(v)
         # 行对象 = 游标类.行对象类(行数据, self)
-        self._内嵌对象.reset()
-        return
+        return self._内嵌对象.reset()
 
     def 行删除(self):
         return self._内嵌对象.deleteRow()
 
     def 行更新(self, 行数据):
         行数据 = [行数据[y] for y in self.字段名称列表]
-        用于更新的列表 = []
-        for k, v in zip(self.字段名称列表, 行数据):
-            if k == "_形状" and v.__class__.__name__ == "形状类":
-                用于更新的列表.append(v._内嵌对象)
-            else:
-                用于更新的列表.append(v)
-        return self._内嵌对象.updateRow(用于更新的列表)
+        return self._内嵌对象.updateRow(行数据)
 
     def 行插入(self, 行数据):
         # print(f"行数据为：{行数据}")
         行数据 = [行数据[y] for y in self.字段名称列表]
-        用于插入的列表 = []
-        for k, v in zip(self.字段名称列表, 行数据):
-            if k == "_形状" and v.__class__.__name__ == "形状类":
-                用于插入的列表.append(v._内嵌对象)
-            else:
-                用于插入的列表.append(v)
-        return self._内嵌对象.insertRow(用于插入的列表)
+        # 日志.输出控制台(行数据)
+        return self._内嵌对象.insertRow(行数据)
 
 
 # class 游标类_用于更新(游标类):
